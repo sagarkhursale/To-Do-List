0af44485ff969e9ca7bc409e5b4270b5225191a5
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-08-31 18:12:25 +0530

    @Dao TaskDao defined

---
+++ app/src/main/java/com/sagar/to_dolist/database/TaskDao.java
@@ -0,0 +1,37 @@
+package com.sagar.to_dolist.database;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface TaskDao {
+
+    @Query("SELECT * FROM task ORDER BY priority")
+    LiveData<List<TaskEntry>> loadAllTasks();
+
+
+    @Insert
+    void insertTask(TaskEntry taskEntry);
+
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    void updateTask(TaskEntry taskEntry);
+
+
+    @Delete
+    void deleteTask(TaskEntry taskEntry);
+
+
+    @Query("SELECT * FROM task WHERE id = :id")
+    LiveData<TaskEntry> loadTaskById(int id);
+
+
+    // END
+}
