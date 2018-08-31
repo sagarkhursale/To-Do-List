31c7447e244bdd37e71d01ca23921419fc070a0c
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-08-31 19:47:08 +0530

    setUp ViewModel

--- app/src/main/java/com/sagar/to_dolist/MainActivity.java
+++ app/src/main/java/com/sagar/to_dolist/MainActivity.java
@@ -1,13 +1,21 @@
 package com.sagar.to_dolist;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+
+import com.sagar.to_dolist.database.TaskEntry;
+
+import java.util.List;
 
 import static android.support.v7.widget.DividerItemDecoration.VERTICAL;
 
@@ -16,6 +24,9 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
     private static final String TAG = MainActivity.class.getSimpleName();
 
     private RecyclerView mRecyclerView;
+
+    private TaskAdapter mAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +40,9 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
         DividerItemDecoration decoration = new DividerItemDecoration(getApplicationContext(), VERTICAL);
         mRecyclerView.addItemDecoration(decoration);
 
+        mAdapter = new TaskAdapter(this, this);
+        mRecyclerView.setAdapter(mAdapter);
+
         FloatingActionButton fabButton = findViewById(R.id.fab);
         fabButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,8 +52,23 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
             }
         });
 
+        setupViewModel();
+
         // end
     }
+
+
+    private void setupViewModel() {
+        MainViewModel viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        viewModel.getTasks().observe(this, new Observer<List<TaskEntry>>() {
+            @Override
+            public void onChanged(@Nullable List<TaskEntry> taskEntries) {
+                Log.d(TAG, "Updating list of tasks from LiveData in ViewModel");
+                mAdapter.setTasks(taskEntries);
+            }
+        });
+    }
+
 
     @Override
     public void onItemClickListener(int itemId) {
