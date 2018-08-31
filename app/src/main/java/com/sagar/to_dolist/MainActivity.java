6dea92cf4ebb766bb07f0200986394f6b0b91564
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-08-31 17:05:43 +0530

    Activity main

--- app/src/main/java/com/sagar/to_dolist/MainActivity.java
+++ app/src/main/java/com/sagar/to_dolist/MainActivity.java
@@ -1,15 +1,43 @@
 package com.sagar.to_dolist;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import static android.support.v7.widget.DividerItemDecoration.VERTICAL;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.recyclerViewTasks);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        DividerItemDecoration decoration = new DividerItemDecoration(getApplicationContext(), VERTICAL);
+        mRecyclerView.addItemDecoration(decoration);
+
+        FloatingActionButton fabButton = findViewById(R.id.fab);
+        fabButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        // end
     }
 
 // END
